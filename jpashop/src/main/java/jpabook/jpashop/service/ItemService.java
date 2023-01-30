8333cd808405c447fa3dc03e372b00b860c24999
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2023-01-30 16:51:00 +0900

    [5. 상품 도메인 개발] 상품 서비스
    - ItemService는 단순히 IemRepository에 위임하는 클래스
    - 트랜잭션 관리를 서비스 계층에서 할 수 있는 장점이 있기 때문에 이런 구현 방식을 채용함

---
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService { //ItemRepository에 위임만 하는 클래스 //위임: 별다른 로직없이 대상을 호출하는 경우 //트랜잭션 관리를 서비스 계층에서 할 수 있는 장점이 있다
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
