fa0c87b4e733be9b2bba2fbeff6286e102c13e1b
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2023-02-03 12:26:32 +0900

    [7. 웹 계층 개발] 회원 목록 조회
    - API를 만들 때에는 엔티티를 넘기면 안 된다.(*)
    1) password 등 정보가 그대로 노출되는 문제
    2) API 스펙이 변해버리는 문제: 엔티티에 로직을 추가하면 그것 때문에 API 스펙이 변해버린다.
    - 타임리프 문법

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,7 +27,7 @@ public class MemberController {
     }
 
     @PostMapping("/members/new") //post는 이 데이터를 실제 등록하는 역할
-    public String create(@Valid MemberForm form, BindingResult result) {
+    public String create(@Valid MemberForm form, BindingResult result) { //Member Entity 를 그대로 쓰기보다는 MemberForm 을 따로 생성하여 사용 (1:1로 매칭되기가 쉽지 않기 때문)
         //회원 이름을 필수로 쓰고 싶다: @Valid 어노테이션을 사용하면 스프링이 javax validation 기능을 쓰는구나라고 인식. 이 폼의 @NotEmpty 조건을 인식
         //validate한 거 다음에 VindingResult가 있으면 오류가 여기 담겨서 코드가 실행이 된다.
         if (result.hasErrors()) {
@@ -42,4 +43,12 @@ public class MemberController {
         return "redirect:/"; //저장이 되고 나면 다시 재 로딩되거나 하면 안 좋기 때문에 리다이렉트로 첫번째 페이지로 보내버린다.
     }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers(); //API를 만들 때에는 이유를 불문하고 엔티티를 넘기면 안 된다.
+        //password가 그대로 노출되는 문제
+        //API 스펙이 변해버린다. 엔티티에 로직을 추가했는데 그것 때문에 API 스펙이 변해버린다. (?) 결과적으로 불안정한 API 스펙이 되어버린다.
+        model.addAttribute("members", members); //이것도 form 객체를 따로 만들거나 DTO를 만드는 것이 더 현명한 것 ㅇ
+        return "members/memberList";
+    }
 }
